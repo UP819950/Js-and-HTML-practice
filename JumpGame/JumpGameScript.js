7193336d7f4015e2832318467ea4991565d87109
UP819950 <up819950@myport.ac.uk>
2020-10-17 18:16:12 +0100

    functionality complete

--- JumpGame/JumpGameScript.js
+++ JumpGame/JumpGameScript.js
@@ -1,9 +1,22 @@
-var character = document.getElementById("character");
-var block = document.getElementById("block");
+let character = document.getElementById("character");
+let block = document.getElementById("block");
 
 function jump() {
-    character.classList.add("animate");
+    if (character.classList != "animate"){
+        character.classList.add("animate");
+    }
+    
     setTimeout(function(){
         character.classList.remove("animate");
     }, 500);
 }
+
+let checkDead = setInterval(function() {
+    let characterTop = parseInt (window.getComputedStyle(character).getPropertyValue("top"));
+    let blockLeft = parseInt (window.getComputedStyle(block).getPropertyValue("left"));
+    if(blockLeft<20 && blockLeft >0 && characterTop >= 130){
+        block.style.animation = "none";
+        block.style.display = "none";
+        alert("Game Over");
+    }
+}, 10);
